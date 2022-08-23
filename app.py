3489710c54f12d0b7fd5b2217c9af3ba1565bd53
WonkaWalrus <54894806+WonkaWalrus@users.noreply.github.com>
2022-08-23 21:36:13 +0900

    add region-specific data and CSS

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -7,12 +7,72 @@ data = data.sort_values(by="date")
 
 app = Dash(__name__)
 
-line_chart = px.line(data, x="date", y="sales", title="Sales of Pink Morsels")
+title_background = "#E8A0BF"
+paper_background = "#FCC5C0"
+plot_background = "#E8A0BF"
+font_color = "#554994"
+
+header = html.H1(
+    "Pink Morsel Visualizer",
+    id="header",
+    style={
+        "background-color": title_background,
+        "border-radius": "20px",
+        "color": font_color,
+        "text-align": "center"
+    }
+)
+
+def create_figure(graph_data):
+    line_chart = px.line(graph_data, x="date", y="sales", title="Sales of Pink Morsels")
+    line_chart.update_layout(
+        paper_bgcolor=paper_background,
+        plot_bgcolor=plot_background,
+        font_color=font_color
+    )
+    return line_chart
+
+graph = dcc.Graph(figure=create_figure(data), id="graph")
+
+region_picker = dcc.RadioItems(
+        ["north", "east", "south", "west", "all"],
+        "all",
+        inline=True,
+        id="region_picker"
+    )
+
+region_container = html.Div(
+    [
+        region_picker
+    ],
+    style={
+        "background-color": title_background,
+        "border-radius": "20px",
+        "color": font_color,
+        "margin-top": "20px",
+        "text-align": "center",
+    }
+)
+
+@app.callback(
+    Output(graph, "figure"),
+    Input(region_picker, "value")
+)
+def update_graph(region):
+    if region == "all":
+        filtered_data = data
+    else:
+        filtered_data = data[data["region"] == region]
+    
+    figure = create_figure(filtered_data)
+    return figure
+
 
 app.layout = html.Div(
     [
-        html.H1("Pink Morsel Visualizer"),
-        dcc.Graph(figure=line_chart)
+        header,
+        graph,
+        region_container
     ]
 )
 
